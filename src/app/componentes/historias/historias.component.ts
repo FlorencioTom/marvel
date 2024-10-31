cb993061bb7b0470343854fcf4982b51e7946bbf
Florencio <florencio.tom@viewnext.com>
2024-10-31 11:51:32 +0100

    Componentes creados

---
+++ src/app/componentes/historias/historias.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historias',
+  standalone: true,
+  imports: [],
+  templateUrl: './historias.component.html',
+  styleUrl: './historias.component.scss'
+})
+export class HistoriasComponent {
+
+}
