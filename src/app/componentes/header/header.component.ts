6e06b57baf720b912fd62e8ccd491c294e9f443c
Florencio <florencio.tom@viewnext.com>
2025-01-28 10:14:40 +0100

    peronaje-individual css titutlo, faltan las demas secciones y areglar el sidebar collapasado

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,20 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
 import { RouterModule } from '@angular/router'; 
 import { AtrasService } from '../../servicios/atras.service';
+import {MatRippleModule} from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TranslateModule,MatButtonModule, MatMenuModule, RouterModule],
+  imports: [TranslateModule,MatButtonModule, MatMenuModule, RouterModule, MatRippleModule, MatIconModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  expand: boolean = false;
   atrasService = inject(AtrasService);
+
   setAtras(){
     this.atrasService.reset();
+  }
+
+  collapse(){
+    this.expand = !this.expand;
   }
 }
